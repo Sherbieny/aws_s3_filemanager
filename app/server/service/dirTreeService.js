85a55870fb2a45331549da9c56fb2fc28274c331
sherbieny <sherbieny@outlook.com>
2024-05-06 22:34:46 +0900

    feat: Add directory tree service
    The code changes introduce a new file `dirTreeService.js` that implements a service to build a directory tree from a list of folders and files. The service utilizes the `crypto` module for generating unique IDs for each folder.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`feat` for a new feature) and provides a concise summary of the changes made.

---
+++ app/server/service/dirTreeService.js
@@ -0,0 +1,50 @@
+// Desc: Service to build a directory tree from a list of folders and files
+
+const crypto = require('crypto');
+
+/**
+ * Build a directory tree from a list of folders and files
+ */
+const buildDirTree = (foldersPaths) => {
+    const folders = {};
+
+    for (const [path, relPath] of Object.entries(foldersPaths)) {
+        // Removing the root path to get the relative path
+        const relativePath = relPath.replace(process.env.S3_BASE_PATH, '');
+
+        // Exploding by "/"
+        const parts = relativePath.split('/');
+
+        // If the path has more than one parts, it's not a root level folder
+        let cur;
+        if (parts.length > 1) {
+            const root = parts.shift(); // Take the root level folder name
+            if (!folders[root]) {
+                folders[root] = createFolder(root, process.env.S3_BASE_PATH + root);
+            }
+            cur = folders[root].children; // Add the rest under the root folder
+        } else {
+            cur = folders;
+        }
+
+        for (const part of parts) {
+            if (part) {  // This will ensure we don't create a folder with an empty key
+                if (!cur[part]) {
+                    cur[part] = createFolder(part, path);
+                }
+                cur = cur[part].children;
+            }
+        }
+    }
+
+    return folders;
+}
+
+const createFolder = (name, path) => {
+    const uniqueId = 'a' + crypto.randomBytes(4).toString('hex'); // This will generate a unique ID not starting with a zero
+    return { id: uniqueId, name, path, children: {} };
+}
+
+module.exports = {
+    buildDirTree
+};
