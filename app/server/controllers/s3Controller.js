120faa20081193ba28dc38d3d01675fc27da9a84
sherbieny <sherbieny@outlook.com>
2024-05-06 22:35:16 +0900

    feat: Add S3Controller functions for interacting with AWS S3 bucket
    The code changes in `s3Controller.js` add several functions to interact with an AWS S3 bucket. These functions include getting the contents of a folder, uploading a file, creating a new folder, deleting a file, getting the sort_order tag data from a file, and setting the sort_order tag data for a file.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`feat` for a new feature) and provides a concise summary of the changes made.

--- app/server/controllers/s3Controller.js
+++ app/server/controllers/s3Controller.js
@@ -1,16 +1,93 @@
 // /server/controllers/s3Controller.js
-const s3 = require('../config/s3');
+const s3Service = require('../services/s3Service');
 
-exports.listFiles = async (req, res) => {
+/**
+ * Get all folder and files in a bucket's path
+ */
+exports.getFolderContents = async (req, res) => {
     try {
-        const data = await s3.listObjectsV2({
-            Bucket: 'your-bucket-name',
-        }).promise();
+        const path = req.query.path;
+        const data = await s3Service.getFolderContents(path);
 
-        res.json(data.Contents);
+        res.json(data);
+
     } catch (error) {
         res.status(500).json({ error: error.toString() });
     }
 };
 
-// Define other controller functions here
+/**
+ * Upload a file to a bucket
+ */
+exports.uploadFile = async (req, res) => {
+    try {
+        const file = req.file;
+        const path = req.body.path;
+        const data = await s3Service.uploadFile(path, file);
+
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
+
+/**
+ * Create new folder in a bucket
+ */
+exports.createFolder = async (req, res) => {
+    try {
+        const path = req.body.path;
+        const data = await s3Service.createFolder(path);
+
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
+
+/**
+ * Delete a file from a bucket
+ */
+exports.deleteFile = async (req, res) => {
+    try {
+        const path = req.body.path;
+        const data = await s3Service.deleteFile(path);
+
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
+
+/**
+ * Get sort_order tag data from a file in the S3 bucket.
+ */
+exports.getSortOrder = async (req, res) => {
+    try {
+        const path = req.query.path;
+        const sortOrder = await s3Service.getSortOrder(path);
+
+        res.json(sortOrder);
+
+    }
+    catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
+
+/**
+ * Set sort_order tag data to a file in the S3 bucket.
+ */
+exports.setSortOrderTag = async (req, res) => {
+    try {
+        const path = req.body.path;
+        const sortOrder = req.body.sortOrder;
+
+        const data = await s3Service.setSortOrderTag(path, sortOrder);
+
+        res.json(data);
+    }
+    catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+};
